60a81424799aaaaf2d00c785d725ee483ba0605f
Andrew <andrii.matukhno@gmail.com>
2018-11-16 01:11:25 +0100

    test

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
-  <div id="app">
-    <img width="25%" src="./assets/logo.png">
-    <HelloWorld/>
-  </div>
+  <div id="app"><img width="25%" src="./assets/logo.png" /> <HelloWorld /></div>
 </template>
 
 <script>
